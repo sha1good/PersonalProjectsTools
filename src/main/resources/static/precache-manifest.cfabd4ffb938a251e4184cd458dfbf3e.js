a176636ba62476b434500f3cf1aaef7e56a5e272
Sheriff <adebisisheriff572@gmail.com>
2020-05-26 03:10:41 +0100

    Adding Static React Files

---
+++ src/main/resources/static/precache-manifest.cfabd4ffb938a251e4184cd458dfbf3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add47f61982f888d4e7cab0bbeaa035d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e144622666f9ca0ddfee",
+    "url": "/static/css/2.e4798c98.chunk.css"
+  },
+  {
+    "revision": "d71567cdd68df765daa3",
+    "url": "/static/css/main.5fdbc097.chunk.css"
+  },
+  {
+    "revision": "e144622666f9ca0ddfee",
+    "url": "/static/js/2.1b717208.chunk.js"
+  },
+  {
+    "revision": "80097c1312432faada68f848f38927c4",
+    "url": "/static/js/2.1b717208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d71567cdd68df765daa3",
+    "url": "/static/js/main.2bcf6a87.chunk.js"
+  },
+  {
+    "revision": "c42518a57ae88aee800e",
+    "url": "/static/js/runtime-main.4a66c584.js"
+  }
+]);
